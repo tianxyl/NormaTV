9f179f574e154d4d53496bb2e9a3398a96096098
tianxyl <tianxyl@163.com>
2025-07-15 23:24:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,10 +22,14 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    dyttzy: {
+    dbzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-        name: '电影天堂资源',
+        name: '蓝光',
         detail: 'http://caiji.dyttzyapi.com', 
+    },
+    dyttzy: {
+        api: 'https://dbzy.tv/api.php/provide/vod',
+        name: '豆瓣资源',
     },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
@@ -73,10 +77,6 @@ const API_SITES = {
         api: 'https://jszyapi.com/api.php/provide/vod',
         name: '极速资源',
         detail: 'https://jszyapi.com', 
-    },
-    dbzy: {
-        api: 'https://dbzy.tv/api.php/provide/vod',
-        name: '豆瓣资源',
     },
     mozhua: {
         api: 'https://mozhuazy.com/api.php/provide/vod',
